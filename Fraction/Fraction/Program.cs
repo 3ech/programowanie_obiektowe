fa7fd13174139d8138f848eac0d67f027312644c
piotr.czech <piotr.czech@PK5-19>
2022-03-05 19:23:35 +0100

    ostateczne

---
+++ Fraction/Fraction/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Fraction
+{
+    class Fraction
+    {
+         static void Main(string[] args)
+         {
+            Fraction j = new Fraction(8, 4);
+            Fraction k = new Fraction(j);
+
+
+            Fraction[] ff = new Fraction[]
+            {
+                new Fraction(8, 4),
+                new Fraction(8, 24),
+                new Fraction(8, 14)
+            };
+
+
+
+             System.Console.WriteLine(k);
+             System.Console.WriteLine("Hello World!");
+         }
+
+        public int Licznik { get; private set; }
+        public int Mianownik { get; private set; }
+
+        //private int licznik;
+        //private int mianownik;
+        public Fraction(int licznik, int mianownik)
+        {
+            this.Licznik = licznik;
+            this.Mianownik = mianownik;
+
+
+        }
+        public Fraction(Fraction previousFraction)
+        {
+            Licznik = previousFraction.Licznik;
+            Mianownik = previousFraction.Mianownik;
+
+        }
+        /*
+        public int Licznik
+        {
+            get { return licznik;  }
+        }
+
+        public int getLicznik()
+        {
+           return licznik;
+        }
+        */
+
+        public override string ToString()
+        {
+            //return base.ToString();
+
+            return Licznik + " / " + mianownik;
+        }
+    }
+}
