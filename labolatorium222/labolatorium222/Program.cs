8d0bb2ac3e343a6afba676558375fbe149536109
piotr.czech <piotr.czech@PK5-19>
2022-03-19 18:11:17 +0100

    testlab2

---
+++ labolatorium222/labolatorium222/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labolatorium222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
